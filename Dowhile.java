29e319f39f6427e399b0ef7c1fa181f4f688c508
VishnuReddy001 <msvvr9866@gmail.com>
2023-12-12 12:19:26 +0530

    java examples

---
+++ Dowhile.java
@@ -0,0 +1,10 @@
+public class Dowhile {
+    public static void main(String[] args) {
+        int i = 1;//Atleast it runs 1 time.
+        do
+        {
+            System.out.println("hello- "+ i);
+            i++;
+        }while(i<=5);
+    }
+}
---
+++ Whileexample.java
@@ -0,0 +1,23 @@
+// public class Whileexample {
+//     public static void main(String[] args) {
+//         int i = 1;
+//         while(i<=4){
+//             System.out.println("Hello java - "+ i);
+//             i++;
+//         }
+//     }
+// }
+public class Whileexample {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=2){
+            System.out.println("Hello - "+ i);
+            int j = 1;
+            while(j<=3){
+                System.out.println("JAVA - " + j);
+                j++;
+            }
+            i++;
+        }
+    }
+}
