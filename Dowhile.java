207488e49ac1b5b28f8fa7b8f2985a2d0de64897
VishnuReddy001 <msvvr9866@gmail.com>
2023-12-12 12:30:16 +0530

    undated

--- Dowhile.java
+++ Dowhile.java
@@ -1,4 +1,5 @@
-public class Dowhile {
+public class Dowhile 
+{
     public static void main(String[] args) {
         int i = 1;//Atleast it runs 1 time.
         do
---
+++ adding.java
@@ -0,0 +1,8 @@
+public class adding {
+    public static void main(String[] args) {
+        int i = 19;
+    int a = 13;
+    long w = (i+a) * (i-a);
+    System.out.println(w);   
+    }
+}
